fed0e8bf31a42e4753fde7d7b1a1feeebbb2d4eb
Damien CAYE <caye.damien@gmail.com>
2024-02-26 12:19:52 +0100

    Finished MenuTwo component + Modified MainComponent

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,17 +1,27 @@
 <template>
-    <v-container class="fill-height">
-        <v-responsive class="align-center fill-height">
-            <TitleOne/>
-            <MenuOne/>
-            <ListePersonnel/>
-            <NextButton/>
-        </v-responsive>
+    <v-container>
+        <v-col>
+            <v-row>
+                <MenuTwo />
+                <TitleOne />
+            </v-row>
+            <v-row>
+                <MenuOne />
+            </v-row>
+            <v-row>
+                <ListePersonnel />
+            </v-row>
+            <v-row>
+                <NextButton />
+            </v-row>
+        </v-col>
     </v-container>
 </template>
 
 <script setup>
 import TitleOne from './TitleOne.vue';
 import MenuOne from './MenuOne.vue';
+import MenuTwo from './MenuTwo.vue';
 import ListePersonnel from './ListePersonnel.vue';
 import NextButton from './NextButton.vue';
 //
